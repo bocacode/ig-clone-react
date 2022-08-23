a993303d711332693f0664fb36f7eb66cbeb4227
Damian Montero <photoca@hotmail.com>
2022-08-23 16:39:32 -0400

    now wiith likes

--- src/services/photoServices.ts
+++ src/services/photoServices.ts
@@ -3,7 +3,7 @@ import { Photo } from "../models/photo";
 const photoAPIUrl = "http://localhost:5001/photos/"
 
 
-async function updateLike (photoId: string): Promise<number> {
+export async function updateLike (photoId: string): Promise<number> {
 
     // PATCH /photos/{PHOTO_ID}  with a body of { likes: 1}
    // "http://localhost:5001/photos/87438734873847348"
@@ -13,10 +13,11 @@ async function updateLike (photoId: string): Promise<number> {
         body: JSON.stringify({ likes: 1})
     })
     const photo: Photo = await fetchPhotos.json()
-    return photo.likes || 0
+    const newLikes: number = photo.likes || 0
+    return newLikes + 1
 }
 
-async function getPhotos() : Promise<Photo[]> {
+export async function getPhotos() : Promise<Photo[]> {
     try {
     const fetchPhotos = await fetch(photoAPIUrl)
     const photoList: Photo[] = await fetchPhotos.json()
